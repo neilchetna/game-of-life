e565e7a0f9336ca7dd66644a39edb25b709bfd8b
neilchetna <neelchetna18@gmail.com>
2022-05-24 00:45:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import produce from "immer";
+import { useState, useCallback } from "react";
+
+const numRows = 85;
+const numCols = 40;
 
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numCols; i++) {
+      rows.push(Array.from(Array(numRows), () => 0));
+    }
+    return rows;
+  });
+  const [running, setRunning] = useState(false);
+
+  const handleGrid = useCallback(() => {
+    setTimeout(handleGrid, 1000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button>{running ? "start" : "stop"}</button>
+      {grid.map((row, i) => (
+        <div key={i} style={{ display: "flex", felxDirection: "column" }}>
+          {row.map((col, j) => (
+            <div
+              key={j}
+              onClick={() => {
+                const newState = produce(grid, (draft) => {
+                  draft[i][j] = grid[i][j] ? 0 : 1;
+                });
+                setGrid(newState);
+              }}
+              style={{
+                border: "1px solid black",
+                padding: "10px",
+                backgroundColor: col ? "black" : null,
+              }}
+            ></div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
