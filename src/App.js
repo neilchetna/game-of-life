aa9c3290c9b06c5706fa77d554514be6372d052f
neilchetna <neelchetna18@gmail.com>
2022-05-26 00:25:59 +0530

    :art: Tailwindcss for better ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,93 @@
 import produce from "immer";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef } from "react";
 
-const numRows = 85;
-const numCols = 40;
+const numRows = 40;
+const numCols = 85;
+
+const neighboursAddress = [
+  [0, -1],
+  [0, 1],
+  [1, 0],
+  [-1, 0],
+  [1, 1],
+  [-1, -1],
+  [1, -1],
+  [-1, 1],
+];
 
 function App() {
-  const [grid, setGrid] = useState(() => {
+  const blanKGrid = () => {
     const rows = [];
-    for (let i = 0; i < numCols; i++) {
-      rows.push(Array.from(Array(numRows), () => 0));
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
     }
     return rows;
-  });
+  };
+  const [grid, setGrid] = useState(blanKGrid);
   const [running, setRunning] = useState(false);
+  const runningRef = useRef(running);
+  runningRef.current = running;
 
   const handleGrid = useCallback(() => {
-    setTimeout(handleGrid, 1000);
+    if (!runningRef.current) {
+      return;
+    }
+
+    console.log(runningRef.current);
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            neighboursAddress.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(handleGrid, 100);
   }, []);
 
   return (
-    <div>
-      <button>{running ? "start" : "stop"}</button>
+    <div className="m-auto">
+      <div>
+        <button
+          className="px-5 bg-slate-400 p-1 m-3 rounded-md ring-1"
+          onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              handleGrid();
+            }
+          }}
+        >
+          {running ? "Stop" : "Start"}
+        </button>
+        <button
+          className="px-5 bg-slate-400 p-1 m-3 rounded-md ring-1"
+          onClick={() => setGrid(blanKGrid)}
+        >
+          Clear
+        </button>
+      </div>
       {grid.map((row, i) => (
-        <div key={i} style={{ display: "flex", felxDirection: "column" }}>
+        <div
+          className="m-auto"
+          key={i}
+          style={{ display: "flex", felxDirection: "column" }}
+        >
           {row.map((col, j) => (
             <div
               key={j}
@@ -35,7 +100,7 @@ function App() {
               style={{
                 border: "1px solid black",
                 padding: "10px",
-                backgroundColor: col ? "black" : null,
+                backgroundColor: col ? "green" : null,
               }}
             ></div>
           ))}
